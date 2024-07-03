aae6c1944f5d40e1dd917ac3e1d93fe3844d1233
Bryan Armenta <a02300296@usu.edu>
2024-07-02 23:32:24 -0600

    Replace typo in area requirement for Triangle

--- src/main/kotlin/Triangle.kt
+++ src/main/kotlin/Triangle.kt
@@ -5,7 +5,7 @@ import kotlin.math.abs
 class Triangle(points: Array<Point>): Shape(points) {
     init {
         require(_points.size == 3) { "Invalid number of points provided for a triangle: ${_points.size} (3 required)"}
-        require(computeArea() == 0.0) { "Triangle cannot have area of 0" }
+        require(computeArea() != 0.0) { "Triangle cannot have area of 0" }
     }
 
     // Utilizes the shoelace formula to calculate the area of a triangle from its vertices
