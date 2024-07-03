e23adb7929f11456814eab137b9d64dc1f66eae7
Bryan Armenta <a02300296@usu.edu>
2024-07-02 20:59:42 -0600

    Implement Rectangle class

---
+++ src/main/kotlin/Rectangle.kt
@@ -0,0 +1,24 @@
+package org.example
+
+class Rectangle(points: Array<Point>): Shape() {
+    // The points can be any two opposite points of the rectangle
+    override var _points: Array<Point> = points
+
+    init {
+        require(_points.size == 2) { "Invalid number of points for rectangle: ${_points.size} (2 required)" } // A rectangle is represented by exactly two points
+        require(computeWidth() != 0.0) { "Rectangle may not have width of 0"}
+        require(computeHeight() != 0.0) { "Rectangle may not have height of 0"}
+    }
+
+    override fun computeArea(): Double {
+        return computeWidth() * computeHeight()
+    }
+
+    private fun computeWidth(): Double {
+        return points[1].x - points[0].x
+    }
+
+    private fun computeHeight(): Double {
+        return points[1].y - points[0].y
+    }
+}
