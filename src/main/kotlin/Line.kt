f54937ec2803b50166d14ff6ac79d115b5f4ad22
Bryan Armenta <a02300296@usu.edu>
2024-07-02 21:03:05 -0600

    Add new functions to compute horizontal and vertical displacement to Line class

--- src/main/kotlin/Line.kt
+++ src/main/kotlin/Line.kt
@@ -1,5 +1,8 @@
 package org.example
 
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 class Line(points: Array<Point>): Movable {
     // Need to use a backing property to ensure points can be modified while returning a clone publicly
     private var _points: Array<Point> = clonePointsArray(points)
@@ -8,8 +11,31 @@ class Line(points: Array<Point>): Movable {
             return clonePointsArray(_points) // Clone to ensure points
         }
 
+    init {
+        require(_points.size == 2) { "Invalid number of points for line: ${_points.size} (2 required)" } // A line must have exactly two points
+        val length = computeLength()
+        require(length != 0.0) { "Line cannot be of length 0"}
+    }
+
+    fun computeSlope(): Double {
+        return computeVertical() / computeHorizontal()
+    }
+
+    fun computeLength(): Double {
+        return sqrt(computeHorizontal().pow(2) + computeVertical().pow(2))
+    }
+
     override fun move(dx: Double, dy: Double) {
         _points.forEach { point -> point.move(dx, dy) }
-        _points.forEach { point -> println("Moved: ${point.x} ${point.y}") }
+    }
+
+    // Compute the horizontal displacement between the points
+    fun computeHorizontal(): Double {
+        return _points[1].x - _points[0].x
+    }
+
+    // Compute the vertical displacement between the points
+    fun computeVertical(): Double {
+        return _points[1].y - _points[0].y
     }
 }
